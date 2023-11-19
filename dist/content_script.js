5764bb1f007af22e2cc35ba888b55583769d2214
Sergey Krivtsov <flashgangsta@gmail.com>
2023-11-19 20:04:43 +0100

    Attemt to access Twitch player API

--- dist/content_script.js
+++ dist/content_script.js
@@ -7,6 +7,8 @@
     const timeOffsetAfterPromoStreamEndInMinutes = 3;
     const hasRedirected = !!sessionStorage.getItem("redirected");
     const isStreamPage = window.location.pathname.startsWith("/tw/");
+    const embedContainer = document.querySelector("#twitch_pl");
+    let embed;
     if (!hasRedirected) {
         log("Init App");
         redirectToIndex();
@@ -51,12 +53,28 @@
             }
         }, delay);
     }
+    function waitTwitchPlayerAppend() {
+        initPlayer();
+        /*setTimeout(() => {
+            const twitchIFrame = document.querySelector("#twitch_pl iframe") as HTMLIFrameElement;
+            if (twitchIFrame) {
+                log("twitch iFrame found, waiting for player will be loaded");
+                twitchIFrame.addEventListener("load",() => {
+                    initPlayer();
+                });
+            } else {
+                log("twitch iFrame not found, restart waiting timer");
+                waitTwitchPlayerAppend();
+            }
+        }, 1000);*/
+    }
     function checkThisStreamCurrentlyPromo() {
         log("SUP: check this stream is currently promo");
         const promoPercentElement = document.querySelector("#promo-percent");
         if (promoPercentElement && getComputedStyle(promoPercentElement).display !== "none") {
             log("SUP: this stream is currently promo");
             startWaitingNextTimePoint();
+            waitTwitchPlayerAppend();
         }
         else {
             log("SUP: this stream is not promo now");
@@ -67,6 +85,46 @@
         log("redirect to index and parse current promo link");
         sessionStorage.setItem("redirected", "true");
         window.location.href = "/";
+    }
+    function initPlayer() {
+        var _a;
+        log("Current window: ", window);
+        //@ts-ignore
+        debugger;
+        if (!((_a = document.defaultView) === null || _a === void 0 ? void 0 : _a.hasOwnProperty("Twitch"))) {
+            log("twitch script not loaded, start waiting timer");
+            log("setting timeout: ", window);
+            setTimeout(() => {
+                log("Timeout handler: ", window);
+                initPlayer();
+            }, 1000);
+        }
+        else {
+            log("twitch script has been loaded, getting player instance");
+            /*//https://dev.twitch.tv/docs/embed/everything/
+            const channelName: string = window.location.pathname.substring(4);
+            embedContainer.innerHTML = "";
+            //@ts-ignore
+            embed = new Twitch.Embed("twitch_pl", {
+                allowfullscreen: true,
+                width: "100%",
+                height: "100%",
+                layout: "video",
+                theme: "dark",
+                channel: channelName,
+                parent: ["stream-up.ru"]
+            });
+            //@ts-ignore
+            embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+                log("Player loaded and ready, set mute, lower quality, and start streaming the channel: " + channelName);
+                //player = embed.getPlayer();
+                const qualities = embed.getQualities();
+                const lowerQuality = qualities[qualities.length - 1];
+                embed.setQuality(lowerQuality.name);
+                embed.setMuted();
+                log("------------------")
+            });*/
+        }
     }
     function log(...data) {
         console.log(`SUM: [${new Date().toLocaleTimeString("ru-RU")}]`, ...data);
--- src/content_script.ts
+++ src/content_script.ts
@@ -3,9 +3,18 @@
         log("is promo page, stop manager");
         return;
     }
+
     const timeOffsetAfterPromoStreamEndInMinutes: number = 3;
     const hasRedirected: boolean = !!sessionStorage.getItem("redirected");
     const isStreamPage: boolean = window.location.pathname.startsWith("/tw/");
+    const embedContainer = document.querySelector("#twitch_pl") as HTMLElement;
+
+    let embed: {
+        addEventListener: (type: string, handler: () => void) => void
+        getQualities: () => { name: string }[];
+        setQuality: (name: string) => void
+        setMuted: () => void
+    };
 
 
     if (!hasRedirected) {
@@ -52,12 +61,29 @@
         }, delay);
     }
 
+    function waitTwitchPlayerAppend() {
+        initPlayer();
+        /*setTimeout(() => {
+            const twitchIFrame = document.querySelector("#twitch_pl iframe") as HTMLIFrameElement;
+            if (twitchIFrame) {
+                log("twitch iFrame found, waiting for player will be loaded");
+                twitchIFrame.addEventListener("load",() => {
+                    initPlayer();
+                });
+            } else {
+                log("twitch iFrame not found, restart waiting timer");
+                waitTwitchPlayerAppend();
+            }
+        }, 1000);*/
+    }
+
     function checkThisStreamCurrentlyPromo(): void {
         log("SUP: check this stream is currently promo");
         const promoPercentElement: HTMLElement = document.querySelector("#promo-percent") as HTMLElement;
         if (promoPercentElement && getComputedStyle(promoPercentElement).display !== "none") {
             log("SUP: this stream is currently promo");
             startWaitingNextTimePoint();
+            waitTwitchPlayerAppend();
         } else {
             log("SUP: this stream is not promo now");
             redirectToIndex();
@@ -70,7 +96,47 @@
         window.location.href = "/";
     }
 
-    function log(...data: any[]):void {
+    function initPlayer(): void {
+        log("Current window: ", window)
+        //@ts-ignore
+        debugger
+        if (!document.defaultView?.hasOwnProperty("Twitch")) {
+            log("twitch script not loaded, start waiting timer");
+            log("setting timeout: ", window)
+            setTimeout((): void => {
+                log("Timeout handler: ", window);
+                initPlayer();
+            }, 1000);
+        } else {
+            log("twitch script has been loaded, getting player instance");
+            /*//https://dev.twitch.tv/docs/embed/everything/
+            const channelName: string = window.location.pathname.substring(4);
+            embedContainer.innerHTML = "";
+            //@ts-ignore
+            embed = new Twitch.Embed("twitch_pl", {
+                allowfullscreen: true,
+                width: "100%",
+                height: "100%",
+                layout: "video",
+                theme: "dark",
+                channel: channelName,
+                parent: ["stream-up.ru"]
+            });
+            //@ts-ignore
+            embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+                log("Player loaded and ready, set mute, lower quality, and start streaming the channel: " + channelName);
+                //player = embed.getPlayer();
+                const qualities = embed.getQualities();
+                const lowerQuality = qualities[qualities.length - 1];
+                embed.setQuality(lowerQuality.name);
+                embed.setMuted();
+                log("------------------")
+            });*/
+        }
+
+    }
+
+    function log(...data: any[]): void {
         console.log(`SUM: [${new Date().toLocaleTimeString("ru-RU")}]`, ...data);
     }
 })();
